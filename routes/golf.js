7642a4f5ba48a618ff4a351f810dac77714e1269
2451134 <briansmith@Brians-MacBook-Pro-4.local>
2019-08-23 08:57:53 +0100

    Added golf route

---
+++ routes/golf.js
@@ -0,0 +1,5 @@
+/* Created by briansmith on 23/08/2019 */
+var express = require("express");
+var router = express.Router();
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
+
+/* golf-1 Home Page */
+// router.get(config.routes.home, function(req, res) {
+//   var data = { title: "Golf Test Routines" };
+//   res.render("home", data);
+// });
 
 module.exports = router;
