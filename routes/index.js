5db12ce029d73cdd6a3863d3995832a444e5513a
2451134 <briansmith@Brians-MacBook-Pro-4.local>
2019-08-24 07:33:26 +0100

    Changed home page

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("index", { title: "Express" });
-});
+// router.get("/", function(req, res, next) {
+//   res.render("index", { title: "Express" });
+// });
 
 /* golf-1 Home Page */
-// router.get(config.routes.home, function(req, res) {
-//   var data = { title: "Golf Test Routines" };
-//   res.render("home", data);
-// });
+router.get("/", function(req, res) {
+  var data = { title: "Golf Test Routines" };
+  res.render("home", data);
+});
 
 module.exports = router;
