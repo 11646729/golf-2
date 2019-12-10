9b1d6d5b3e07cf39e199081e3b60b3224783c6dc
2451134 <briansmith@Brians-MacBook-Pro-4.local>
2019-12-10 13:26:45 +0000

    Copied some files from old project

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,44 @@
 const express = require("express")
+const path = require("path")
+const cookieParser = require("cookie-parser")
+const logger = require("morgan")
+
+const createError = require("http-errors")
+const favicon = require("serve-favicon")
 const cors = require("cors")
+const mongoose = require("mongoose")
 
 require("dotenv").config()
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  autoIndex: false, // Don't build indexes
+  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0,
+  connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4 // Use IPv4, skip trying IPv6
+}
+
+try {
+  // mongoose.connect(mongodb, options)
+  mongoose.connect(process.env.ATLAS_URI, options)
+  mongoose.connection.on(
+    "connected",
+    console.error.bind(console, "Mongodb connected")
+  )
+} catch (error) {
+  console.log("Mongodb connection error")
+}
+
+// Configure mongoose's promise to global promise
+mongoose.promise = global.Promise
 
 const app = express()
 const port = process.env.PORT || 5000
