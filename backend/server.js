99f0c3b9e186df6c21538e09c31b32917625127d
2451134 <bds6052@gmail.com>
2019-12-11 10:19:14 +0000

    Trying to fix MongoDB connection error

--- backend/server.js
+++ backend/server.js
@@ -37,11 +37,11 @@ const options = {
   useCreateIndex: true,
   useFindAndModify: false,
   autoIndex: false, // Don't build indexes
-  poolSize: 10, // Maintain up to 10 socket connections
+  poolSize: 30, // Maintain up to 10 socket connections
   // If not connected, return errors immediately rather than waiting for reconnect
   bufferMaxEntries: 0,
   connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
-  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  socketTimeoutMS: 360000, // Close sockets after 45 seconds of inactivity
   family: 4 // Use IPv4, skip trying IPv6
 }
 
