f60124b8baa64e648fb6fdf67680495fcac3be2b
2451134 <bds6052@gmail.com>
2019-12-13 21:30:38 +0000

    Delete surplus comment

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-// import React, { Component } from "react"
-// //import logo from "./logo.svg"
-// import MapContainer from "./containers/MapContainer"
-
-// // CSS
-// import "./App.css"
-// import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-// import "antd/dist/antd.css"
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-//         </header>
-//         <div className="container h-100">
-//           <MapContainer />
-//         </div>
-//       </div>
-//     )
-//   }
-// }
-
-// export default App
-
 import React, { Component } from "react"
 import { Map, GoogleApiWrapper } from "google-maps-react"
 import { google_maps_api } from "./google_maps"
