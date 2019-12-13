ebb193fcac9dc93288754bdde2ba1b1f95979ed1
2451134 <bds6052@gmail.com>
2019-12-13 13:19:42 +0000

    Trying to enable google maps api - no joy yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+// import React, { Component } from "react"
+// //import logo from "./logo.svg"
+// import MapContainer from "./containers/MapContainer"
+
+// // CSS
+// import "./App.css"
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+// import "antd/dist/antd.css"
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           {/* <img src={logo} className="App-logo" alt="logo" /> */}
+//         </header>
+//         <div className="container h-100">
+//           <MapContainer />
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
+
 import React, { Component } from "react"
-//import logo from "./logo.svg"
-import MapContainer from "./containers/MapContainer"
+import { Map, GoogleApiWrapper } from "google-maps-react"
 
-// CSS
-import "./App.css"
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "antd/dist/antd.css"
+const mapStyles = {
+  width: "100%",
+  height: "100%"
+}
 
-class App extends Component {
+export class MapContainer extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        </header>
-        <div className="container h-100">
-          <MapContainer />
-        </div>
-      </div>
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+          lat: -1.2884,
+          lng: 36.8233
+        }}
+      />
     )
   }
 }
 
-export default App
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyDBfD68OiYf_mqSh9tK2N85VQSG5iF43yI"
+})(MapContainer)
