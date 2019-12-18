347323dcf70881b95ebc3634a67522b9ee432009
2451134 <bds6052@gmail.com>
2019-12-18 10:22:30 +0000

    Updated arrival.js model

--- backend/models/cruiseShippingModels/v1/arrival.js
+++ backend/models/cruiseShippingModels/v1/arrival.js
@@ -4,9 +4,13 @@ var Schema = mongoose.Schema
 
 var ArrivalSchema = new Schema(
   {
+    port_url: {
+      type: String,
+      default: "https://www.cruisemapper.com/ports/belfast-port-114"
+    },
     port_name: { type: String, default: "Belfast" },
     port_locode: { type: String, default: "GBBEL" },
-    //    location: { type: Point, coordinates: [555, 5459] }
+    //    location: { type: Point, coordinates: [555, 5459] },
     arrival_date: { type: String, default: "14 March, 2020" },
     arrival_day: { type: String, default: "Saturday" },
     vessel_name: { type: String, default: "CMV Astoria" },
