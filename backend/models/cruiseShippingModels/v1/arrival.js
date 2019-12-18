fdf45c6a4e74588df27a113da30189fdf18bf07c
2451134 <bds6052@gmail.com>
2019-12-18 10:18:52 +0000

    Added arrival.js file

---
+++ backend/models/cruiseShippingModels/v1/arrival.js
@@ -0,0 +1,26 @@
+var mongoose = require("mongoose")
+
+var Schema = mongoose.Schema
+
+var ArrivalSchema = new Schema(
+  {
+    port_name: { type: String, default: "Belfast" },
+    port_locode: { type: String, default: "GBBEL" },
+    //    location: { type: Point, coordinates: [555, 5459] }
+    arrival_date: { type: String, default: "14 March, 2020" },
+    arrival_day: { type: String, default: "Saturday" },
+    vessel_name: { type: String, default: "CMV Astoria" },
+    eta: { type: Number, default: "03:00" },
+    etd: { type: Number, default: "14:00" },
+    vessel_name_url: {
+      type: String,
+      default: "https://www.cruisemapper.com/ships/CMV-Astoria-821"
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+// Export model
+module.exports = mongoose.model("arrival", ArrivalSchema)
