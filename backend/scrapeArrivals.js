350c3ba6d5357fc6e62578f77563bc5ce95b47c3
2451134 <bds6052@gmail.com>
2020-01-12 10:46:09 +0000

    Fixed scrapeArrivals algorithm

--- backend/scrapeArrivals.js
+++ backend/scrapeArrivals.js
@@ -16,77 +16,81 @@ export async function getVesselArrivals(selectedMonth, selectedYear) {
 
   var vessel_arrival = []
 
-  $(".newDay").each((i, item) => {
-    // Database version
-    const databaseVersion = "1.0"
+  $(".portItemSchedule tr").each((i, item) => {
+    // Ignore the table heading
+    if (i > 0) {
+      // Database version
+      const databaseVersion = "1.0"
 
-    // Port Name
-    const port_name = "Belfast"
+      // Port Name
+      const port_name = "Belfast"
 
-    // Port UN Locode
-    const port_un_locode = "GBBEL"
+      // Port UN Locode
+      const port_un_locode = "GBBEL"
 
-    // Name of Vessel
-    const vessel_shortcruise_name = $(item)
-      .find("a")
-      .text()
+      // Name of Vessel
+      const vessel_shortcruise_name = $(item)
+        .find("a")
+        .text()
 
-    //  Date of Arrival
-    let temp_arrival_date = $(item)
-      .children("td")
-      .html()
-      .replace(/,/, "") // Removes the comma
+      //  Date of Arrival
+      let arrival_date = $(item)
+        .children("td")
+        .children("span")
+        .html()
+        .replace(/,/, "") // Removes the comma
 
-    // Now extract the date string
-    let n = temp_arrival_date.indexOf(selectedYear)
-    const arrival_date = temp_arrival_date.substr(6, n - 2)
+      // // Now extract the date string
+      //      let n = temp_arrival_date.indexOf(selectedYear)
+      //      const arrival_date = temp_arrival_date.substr(6, n - 2)
 
-    // // Expected Time of Arrival
-    let vessel_eta = $(item)
-      .children("td")
-      .next("td")
-      .next("td")
-      .html() // 03:00
+      // // Expected Time of Arrival
+      let vessel_eta = $(item)
+        .children("td")
+        .next("td")
+        .next("td")
+        .html() // 03:00
 
-    // If No Arrival Time Given
-    if (vessel_eta == "") {
-      vessel_eta = "Not Known"
-    } else {
-      vessel_eta = Date.parse(arrival_date + " " + vessel_eta + " GMT")
-      var d = new Date(vessel_eta)
-      vessel_eta = d.toUTCString()
+      // If No Arrival Time Given
+      if (vessel_eta == "") {
+        vessel_eta = "Not Known"
+      } else {
+        vessel_eta = Date.parse(arrival_date + " " + vessel_eta + " GMT")
+        var d = new Date(vessel_eta)
+        vessel_eta = d.toUTCString()
+      }
+
+      // // Expected Time of Departure
+      let vessel_etd = $(item)
+        .children("td")
+        .last("td")
+        .html() // 14:00
+
+      // If No Departure Time Given
+      if (vessel_etd == "") {
+        vessel_etd = "Not Known"
+      } else {
+        vessel_etd = Date.parse(arrival_date + " " + vessel_etd + " GMT")
+        var d = new Date(vessel_etd)
+        vessel_etd = d.toUTCString()
+      }
+
+      // Url of Vessel Web Page
+      const vessel_name_url = $(item)
+        .find("a")
+        .attr("href")
+
+      // Push an object with the data onto our array
+      vessel_arrival.push({
+        databaseVersion,
+        port_name,
+        port_un_locode,
+        vessel_shortcruise_name,
+        vessel_eta,
+        vessel_etd,
+        vessel_name_url
+      })
     }
-
-    // // Expected Time of Departure
-    let vessel_etd = $(item)
-      .children("td")
-      .last("td")
-      .html() // 14:00
-
-    // If No Departure Time Given
-    if (vessel_etd == "") {
-      vessel_etd = "Not Known"
-    } else {
-      vessel_etd = Date.parse(arrival_date + " " + vessel_etd + " GMT")
-      var d = new Date(vessel_etd)
-      vessel_etd = d.toUTCString()
-    }
-
-    // Url of Vessel Web Page
-    const vessel_name_url = $(item)
-      .find("a")
-      .attr("href")
-
-    // Push an object with the data onto our array
-    vessel_arrival.push({
-      databaseVersion,
-      port_name,
-      port_un_locode,
-      vessel_shortcruise_name,
-      vessel_eta,
-      vessel_etd,
-      vessel_name_url
-    })
   })
 
   return vessel_arrival
