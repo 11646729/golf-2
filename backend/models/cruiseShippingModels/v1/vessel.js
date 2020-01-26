d0aa7578a0d101d6706719412220eb6273b6b02d
2451134 <bds6052@gmail.com>
2020-01-26 09:29:33 +0000

    Refactored vessel model

--- backend/models/cruiseShippingModels/v1/vessel.js
+++ backend/models/cruiseShippingModels/v1/vessel.js
@@ -21,11 +21,11 @@ var VesselSchema = new Schema({
   vessel_short_operator: { type: String, default: "Spirit" },
   vessel_long_operator: { type: String, default: "Saga Cruises" },
   vessel_year_built: { type: Number, default: "2019" },
-  vessel_length_metres: { type: String, default: "236 m" },
-  vessel_width_metres: { type: String, default: "31 m" },
-  vessel_gross_tonnage: { type: String, default: "58250 gt" },
-  vessel_average_speed_knots: { type: String, default: "23 kn" },
-  vessel_max_speed_knots: { type: String, default: "23 kn" },
+  vessel_length_metres: { type: Number, default: "236" },
+  vessel_width_metres: { type: Number, default: "31" },
+  vessel_gross_tonnage: { type: Number, default: "58250" },
+  vessel_average_speed_knots: { type: Number, default: "23" },
+  vessel_max_speed_knots: { type: Number, default: "23" },
   vessel_average_draught_metres: { type: Number, default: "7.9" },
   vessel_imo_number: { type: Number, default: "8217881" },
   vessel_mmsi_number: { type: Number, default: "311000343" },
@@ -33,10 +33,6 @@ var VesselSchema = new Schema({
   vessel_typical_passengers: { type: String, default: "1000" },
   vessel_typical_crew: { type: String, default: "530" }
 })
-
-//
-// TODO - Photo
-//
 
 // Export model
 module.exports = mongoose.model("vessel", VesselSchema)
