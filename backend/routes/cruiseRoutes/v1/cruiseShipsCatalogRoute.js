3d8a04dac176ba104f5105243d8be5da19a0f62c
2451134 <bds6052@gmail.com>
2020-01-27 20:34:55 +0000

    Minor comments added

--- backend/routes/cruiseRoutes/v1/cruiseShipsCatalogRoute.js
+++ backend/routes/cruiseRoutes/v1/cruiseShipsCatalogRoute.js
@@ -7,16 +7,18 @@ var router = express.Router()
 const port_arrivals_controller = require("../../../controllers/cruiseShippingControllers/portArrivalsController")
 const vessel_controller = require("../../../controllers/cruiseShippingControllers/vesselController")
 
-/// CRUISE ROUTES ///
+/// PortArrivals Routes ///
 
 // Get catalogue home page
 router.get("/", port_arrivals_controller.index_get)
 
-// Get all port arrivals - only Belfast at present
+// Get all port arrivals from the database - only Belfast at present
 router.get("/portArrivals", port_arrivals_controller.port_arrivals_get)
 
 // Post a single arrival to the database
 router.post("/portArrivals/add", port_arrivals_controller.port_arrivals_add)
+
+/// Vessels Routes ///
 
 // Get all vessels from the database
 router.get("/vessels", vessel_controller.vessel_get)
