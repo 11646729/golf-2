1560701279fc7e40db14fbdd883d1924b3511d6c
2451134 <bds6052@gmail.com>
2020-05-15 09:22:34 +0100

    Added extended InfoWindows

--- frontend/src/components/FR GoogleMapContainer.js
+++ frontend/src/components/FR GoogleMapContainer.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react"
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react"
+import { Card, CardContent, CardMedia, Typography } from "@material-ui/core"
 
-const style = {
+const styles = {
   map: {
     width: "100%",
     height: "100%",
@@ -12,13 +13,48 @@ const style = {
     width: "100%",
     height: "100%",
   },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9,
+    marginTop: "30",
+  },
+  // mapConfig: {
+  //   width: "100%",
+  //   height: "100%",
+  //   centerAroundCurrentLocation: false,
+  //   visible: true,
+  //   fullscreenControl: false,
+
+  //   mapTypeId: "hybrid",
+  //   zoomControl: true,
+  //   panControl: false,
+  //   mapTypeControl: false,
+  //   scaleControl: false,
+  //   overviewMapControl: false,
+  //   rotateControl: false,
+  //   disableDefaultUI: true,
+  //   keyboardShortcuts: false,
+  //   draggable: false,
+  //   disableDoubleClickZoom: true,
+  //   zoomControlOptions: {
+  //     style: google.maps.ZoomControlStyle.SMALL,
+  //     position: google.maps.ControlPosition.RIGHT_TOP,
+  //   },
+  //   scrollwheel: false,
+  // },
 }
 
 export class MapContainer extends Component {
   state = {
     showingInfoWindow: false,
     activeMarker: {},
+    image: "static/images/Bosphorus.jpg",
+    photoTitle: "Istanbul Bridge Photo",
+    title: "Istanbul",
+    description:
+      "Istanbul is a major city in Turkey that straddles Europe and Asia across the Bosphorus Strait. Its Old City reflects cultural influences of the many empires that once ruled here.",
   }
+
   onMarkerClick = (props, marker, e) =>
     this.setState({
       selectedPlace: props,
@@ -54,8 +90,10 @@ export class MapContainer extends Component {
           lat: 42.39,
           lng: -72.52,
         }}
-        style={style.map}
+        style={styles.map}
         zoom={14}
+        // mapTypeId={"hybrid"}
+        streetViewControl={false}
         // bounds={bounds}
       >
         <Marker onClick={this.onMarkerClick} name={"Current location"} />
@@ -65,10 +103,19 @@ export class MapContainer extends Component {
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
         >
-          <div>
-            {/* <h1>{this.state.selectedPlace.name}</h1> */}
-            <h1>Here I am</h1>
-          </div>
+          <Card>
+            <CardMedia
+              style={styles.media}
+              image={this.state.image}
+              title={this.state.photoTitle}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {this.state.title}
+              </Typography>
+              <Typography component="p">{this.state.description}</Typography>
+            </CardContent>
+          </Card>
         </InfoWindow>
       </Map>
     )
