745f49a6ef9f1949ec7a0be2aecb1a33ff2ea6e7
2451134 <bds6052@gmail.com>
2020-10-15 11:33:59 +0100

    Fixed ESLint react version error

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -18,5 +18,15 @@ module.exports = {
         "react"
     ],
     "rules": {
-    }
+    },
+    "settings": {
+        "react": {
+            "createClass": "createReactClass", // Regex for Component Factory to use,
+            // default to "createReactClass"
+            "pragma": "React",  // Pragma to use, default to "React"
+            "fragment": "Fragment",  // Fragment to use (may be a property of <pragma>), default to "Fragment"
+            "version": "latest", // React version. "detect" automatically picks the version you have installed.
+            "flowVersion": "0.53" // Flow version
+        },
+    },
 };
--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -26,4 +26,14 @@ module.exports = {
         "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
         // "prettier/prettier": ["error"],
     },
+    "settings": {
+        "react": {
+            "createClass": "createReactClass", // Regex for Component Factory to use,
+            // default to "createReactClass"
+            "pragma": "React",  // Pragma to use, default to "React"
+            "fragment": "Fragment",  // Fragment to use (may be a property of <pragma>), default to "Fragment"
+            "version": "latest", // React version. "detect" automatically picks the version you have installed.
+            "flowVersion": "0.53" // Flow version
+        },
+    },
 }
