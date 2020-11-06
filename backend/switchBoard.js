8b66bf7f4ba677cb6e667d8d36e63e408a511973
2451134 <bds6052@gmail.com>
2020-11-06 10:13:34 +0000

    Delete unused file & reloaded gtfs & geojson data

--- backend/switchBoard.js
+++ backend/switchBoard.js
@@ -3,10 +3,8 @@ import { directDeleteAll as deleteAllPortArrivals } from "./controllers/cruiseCo
 import { directDeleteAll as deleteAllVesselDetails } from "./controllers/cruiseControllers/v1/vesselDetailsController"
 import { directDeleteAll as deleteAllTemperatures } from "./controllers/weatherControllers/v1/weatherController"
 import { runCron } from "./cronRoutines"
-import { importGtfsToSQLite } from "./importGftsToSQLite"
 import { createReducedShapeData } from "./createReducedShapeData"
 import { importTripIdReducedShapeData } from "./importTripIdIntoReducedShapeData"
-// import { convertGtfsDataToGeojson } from "./convertGtfsDataToGeojson"
 import { importTranslinkStopData } from "./importTranslinkStopData"
 import { importTranslinkShapeData } from "./importTranslinkShapeData"
 import { importAndReduceTranslinkShapeData } from "./importAndReduceTranslinkShapeData"
@@ -23,34 +21,30 @@ export const runSwitchboard = (io) => {
   // Using socket.io for realtime
   // io.on("connection", (socket) => {
   //   console.log("Client Connected")
-
-  //   // Start listening for browser position data
-  //   socket.on("fetchLocation", (pos) => {
-  //     console.log(
-  //       "Start listening to the fetchLocation event in the switchboard file"
-  //     )
-  //   })
-
+  // Start listening for browser position data
+  // socket.on("fetchLocation", (pos) => {
+  //   console.log(
+  //     "Start listening to the fetchLocation event in the switchboard file"
+  //   )
+  // })
   // Fetch data Daily at 07:00
   // cron.schedule("0 7 * * *", () => {
   // cron.schedule("*/15 * * * *", () => {
   // console.log("Started getting Vessel Arrivals & Details Scraping")
   // deleteAllPortArrivals()
   // deleteAllVesselDetails()
+  // console.log("Vessel Arrivals & Details Scraping done at " + Date.now())
   // deleteAllTemperatures()
   // runCron()
-  // importGtfsToSQLite()
+
   convertGtfsToGeoJSON()
   // createReducedShapeData()
   // importTripIdReducedShapeData()
   // console.log("Back in the switchBoard function")
-  // convertGtfsDataToGeojson()
   // importTranslinkStopData()
   // importTranslinkShapeData()
   // importAndReduceTranslinkShapeData()
-  // console.log("Vessel Arrivals & Details Scraping done at " + Date.now())
   // })
-
   // Fetch data every 1 minutes
   // cron.schedule("*/1 * * * *", () => {
   //   getAndSaveDarkSkiesData().then((result) => {
@@ -59,7 +53,6 @@ export const runSwitchboard = (io) => {
   //     })
   //   })
   // })
-
   // Save data to database every 10 minutes
   // cron.schedule("*/10 * * * *", () => {
   //   deleteAllBusStops()
@@ -67,17 +60,13 @@ export const runSwitchboard = (io) => {
   //     console.log("In switchboard function saveGolfCourseDataToDatabase")
   //   })
   // })
-
   // socket.off("fetchLocation", () => {
   //   console.log(
   //     "Stop listening to the fetchLocation event in the switchboard file"
   //   )
   // })
-
   // socket.on("disconnect", () => {
   //   console.log("Client Disconnected")
   // })
   // })
 }
-
-// "skipImport": true
