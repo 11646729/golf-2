3cbc72a5e7c37ae0fa5095a91a2b14931ae976e9
2451134 <bds6052@gmail.com>
2022-09-08 09:55:17 +0100

    Changed cheerio import to eliminate warning

--- backend/controllers/vesselController.js
+++ backend/controllers/vesselController.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import cheerio from "cheerio"
+import * as cheerio from "cheerio"
 import moment from "moment"
 
 import { openSqlDbConnection, closeSqlDbConnection } from "../fileUtilities.js"
