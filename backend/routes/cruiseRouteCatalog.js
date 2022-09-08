fdcd9547a42f9cfd9390b2fb17a4777bf6baeaef
2451134 <bds6052@gmail.com>
2022-09-08 10:20:00 +0100

    Remove too many comment lines

--- backend/routes/cruiseRouteCatalog.js
+++ backend/routes/cruiseRouteCatalog.js
@@ -14,74 +14,48 @@ import { fetchPortArrivalsAndVessels } from "../cruiseScrapingRoutines.js"
 
 var cruiseRouter = express.Router()
 
-// -------------------------------------------------------
 // Cruise Routes
 // GET catalogue home page
-// -------------------------------------------------------
 cruiseRouter.get("/", index)
 
-// -------------------------------------------------------
 // Prepare the portarrivals table in the SQL database
-// -------------------------------------------------------
 cruiseRouter.post("/preparePortArrivalsTable", prepareEmptyPortArrivalsTable)
 
-// -------------------------------------------------------
 // GET all port arrivals from the database
-// -------------------------------------------------------
 cruiseRouter.get("/portArrivals", getPortArrivals)
 
-// -------------------------------------------------------
 // GET a port arrival by id
-// -------------------------------------------------------
 // cruiseRouter.get("/portArrivals/:id", getPortArrival)
 
-// -------------------------------------------------------
 // POST a port arrival to the database
-// -------------------------------------------------------
 cruiseRouter.post("/portArrivals", savePortArrival)
 
-// -------------------------------------------------------
 // PUT a port arrival by id
-// -------------------------------------------------------
 // cruiseRouter.put("/portArrivals/:id", putPortArrival)
 
-// -------------------------------------------------------
 // Prepare the vessels table in the SQL database
-// -------------------------------------------------------
 cruiseRouter.post("/prepareVesselsTable", prepareEmptyVesselsTable)
 
-// -------------------------------------------------------
-/// Vessel Routes ///
+// Vessel Routes
 // GET all vessels from the database
-// -------------------------------------------------------
 // cruiseRouter.get("/vessel", getVessels)
 
-// -------------------------------------------------------
 // GET a vessel by id
-// -------------------------------------------------------
 // cruiseRouter.get("/vessel/:id", getVessel)
 
-// -------------------------------------------------------
 // GET all vessel positions
-// -------------------------------------------------------
 cruiseRouter.get("/vesselPositions", getVesselPosition)
 
-// -------------------------------------------------------
 // POST a vessel to the database
-// -------------------------------------------------------
 cruiseRouter.post("/vessel", saveVessel)
 
-// -------------------------------------------------------
-// PUT a vessel by id
-// -------------------------------------------------------
-// cruiseRouter.put("/vessel/:id", putVessel)
-
-// -------------------------------------------------------
-// PUT all Port Arrivals & Vessels data into the SQL database
-// -------------------------------------------------------
+// POST all Port Arrivals & Vessels data into the SQL database
 cruiseRouter.post(
   "/importPortArrivalsAndVesselsData",
   fetchPortArrivalsAndVessels
 )
+
+// PUT a vessel by id
+// cruiseRouter.put("/vessel/:id", putVessel)
 
 export default cruiseRouter
