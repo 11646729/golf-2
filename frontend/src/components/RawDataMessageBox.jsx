9c52c39b571ab0aee03f9839d3d4d592e1e91bf1
2451134 <bds6052@gmail.com>
2022-10-13 09:19:46 +0100

    Adding textarea in RawDataMessageBox

--- frontend/src/components/RawDataMessageBox.jsx
+++ frontend/src/components/RawDataMessageBox.jsx
@@ -7,27 +7,35 @@ import Title from "./Title"
 const RawDataMessageBoxTitle = "Status Messages"
 const RawDataMessageString = "Here is the first message"
 
-const RawDataMessageBoxTitleContainer = styled.div`
-  margin-top: 35px;
-  margin-left: 20px;
-  margin-right: 20px;
-  width: "97%";
+const RawDataMessageBoxContainer = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  padding-left: 20px;
+  padding-top: 30px;
+`
+
+const RawDataMessageBoxStyle = styled.textarea`
+  margin-left: 30px;
+  width: 92%;
+  height: 90%;
+  border: 1px solid lightgray;
+  font-size: 13px;
 `
 
 const RawDataMessageBox = (props) => {
-  //   const { messageString } = props
+  // const { messageString } = props
 
-  //   RawDataMessageBox.propTypes = {
-  //     messageString: PropTypes.string,
-  //   }
+  // RawDataMessageBox.propTypes = {
+  //   messageString: PropTypes.string,
+  // }
 
   return (
-    <div>
-      <RawDataMessageBoxTitleContainer>
-        <Title>{RawDataMessageBoxTitle}</Title>
+    <RawDataMessageBoxContainer>
+      <Title>{RawDataMessageBoxTitle}</Title>
+      <RawDataMessageBoxStyle rows="6">
         {RawDataMessageString}
-      </RawDataMessageBoxTitleContainer>
-    </div>
+      </RawDataMessageBoxStyle>
+    </RawDataMessageBoxContainer>
   )
 }
 
