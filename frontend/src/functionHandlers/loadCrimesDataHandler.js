e650f005d53fc88e45cda9a81b7b1cda3c2d9f6a
2451134 <bds6052@gmail.com>
2022-10-23 12:03:34 +0100

    Refactor loadCrimesDataHandler

--- frontend/src/functionHandlers/loadCrimesDataHandler.js
+++ frontend/src/functionHandlers/loadCrimesDataHandler.js
@@ -1,14 +1,15 @@
-import axios from "axios"
-
-export const loadCrimesDataHandler = () => {
-  alert("In loadCrimesDataHandler function")
-}
+import { getCommonData } from "./axios-common"
 
 // -------------------------------------------------------
 // Function to fetch all Crimes data
 // -------------------------------------------------------
-export const getCrimesData = (url) =>
-  axios
-    .get(url)
-    .then((response) => response.data)
-    .catch((err) => console.log(err))
+export const getCrimesData = (url) => getCommonData(url)
+
+// -------------------------------------------------------
+// Function to fetch all Crimes data into the SQL database
+// -------------------------------------------------------
+export const loadCrimesDataHandler = () => {
+  alert("In loadCrimesDataHandler function")
+}
+
+export { getCrimesData as default }
