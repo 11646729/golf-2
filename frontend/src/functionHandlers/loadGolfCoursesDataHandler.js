82c5e26819e9c81367441b556dd2465ba028d8c0
2451134 <bds6052@gmail.com>
2022-10-23 11:58:14 +0100

    Refactor loadGolfCoursesDataHandler

--- frontend/src/functionHandlers/loadGolfCoursesDataHandler.js
+++ frontend/src/functionHandlers/loadGolfCoursesDataHandler.js
@@ -1,38 +1,18 @@
-// import axios from "axios"
 import { getCommonData, postCommonData } from "./axios-common"
 
 // -------------------------------------------------------
 // Function to prepare the golfcourses table in the SQL database
 // -------------------------------------------------------
-// export const prepareGolfCoursesTable = (url) =>
-//   axios
-//     .post(url)
-//     // .then(() => console.log("Empty golfcourses table prepared"))
-//     .catch((err) => console.log(err))
-
-export const prepareGolfCoursesTable = (url) => postCommonData(url)
+const prepareGolfCoursesTable = (url) => postCommonData(url)
 
 // -------------------------------------------------------
 // Function to instruct backend to load Golf Club Data into the database
 // -------------------------------------------------------
-// export const initialImportOfGolfCoursesData = (url) =>
-//   axios
-//     .get(url)
-//     .then(() => console.log("Golf Course data loaded into the SQL database"))
-//     .catch((err) => console.log(err))
-
-export const initialImportOfGolfCoursesData = (url) => getCommonData(url)
+const initialImportOfGolfCoursesData = (url) => getCommonData(url)
 
 // -------------------------------------------------------
 // Function to fetch all Golf Course data - DON'T TRY TO REFACTOR THIS
 // -------------------------------------------------------
-// export const getGolfCoursesData = (url) =>
-//   axios
-//     .get(url)
-//     // .get("http://localhost:4000/api/golf/getGolfCourses")
-//     .then((response) => response.data)
-//     .catch((err) => console.log(err))
-
 export const getGolfCoursesData = (url) => getCommonData(url)
 
 // -------------------------------------------------------
