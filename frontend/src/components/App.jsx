dafdfb8fe776a63d7978c9043240edd3d751b0f7
2451134 <bds6052@gmail.com>
2023-03-27 09:20:23 +0100

    Add Framework for Canvas Test

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -9,6 +9,7 @@ import GolfCoursesPage from "../pages/GolfCoursesPage"
 import NearbyCrimesPage from "../pages/NearbyCrimesPage"
 import CruisesPage from "../pages/CruisesPage"
 import BusRoutesPage from "../pages/BusRoutesPage"
+import CanvasTestPage from "../pages/CanvasTestPage"
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path="nearbycrimespage" element={<NearbyCrimesPageLink />} />
           <Route path="cruisespage" element={<CruisesPageLink />} />
           <Route path="busroutespage" element={<BusRoutesPageLink />} />
+          <Route path="canvastestpage" element={<CanvasTestPageLink />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
@@ -57,6 +59,10 @@ function NearbyCrimesPageLink() {
 
 function BusRoutesPageLink() {
   return <BusRoutesPage />
+}
+
+function CanvasTestPageLink() {
+  return <CanvasTestPage />
 }
 
 function NotFound() {
---
+++ frontend/src/pages/CanvasTestPage.jsx
@@ -0,0 +1,10 @@
+import React, { memo } from "react"
+
+// -------------------------------------------------------
+// React Controller component
+// -------------------------------------------------------
+const CanvasTestPage = () => {
+  return <h3>Hello World!</h3>
+}
+
+export default memo(CanvasTestPage)
