b6d62a24a9668cadd85e5b70dceb2c28ccb5e42c
2451134 <bds6052@gmail.com>
2023-04-04 09:55:08 +0100

    Remove unrequired code

--- frontend/src/cards/RawDataLoadCard.jsx
+++ frontend/src/cards/RawDataLoadCard.jsx
@@ -11,10 +11,6 @@ import {
 } from "@material-ui/core"
 
 const useStyles = makeStyles((theme) => ({
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
   card: {
     height: "100%",
     display: "flex",
